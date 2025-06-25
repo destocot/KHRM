ebe79f029bb86fb340fa6cc6561a9d6f8318613a
Khurram Ali <khurramcali@gmail.com>
2025-06-24 21:59:45 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <div className='container py-16'>
+        <h1 className='text-3xl font-bold tracking-tight sm:text-4xl'>Khurram Ali</h1>
+      </div>
+    </main>
+  )
+}
